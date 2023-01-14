8f9817cccb585290e47658839e9bbfb03075f889
sh-navid <shamshirsaz.navid@live.com>
2023-01-14 15:23:07 +0330

    update

--- index.sw.js
+++ index.sw.js
@@ -1,7 +1,7 @@
 // Helper link
 // https://bitsofco.de/setting-up-a-basic-service-worker/
 
-const cacheVersion = 'v1.34';
+const cacheVersion = 'v1.36';
 const cacheFiles = [
 	'./',
 	'./assets/icons/1.png?v=' + cacheVersion,
