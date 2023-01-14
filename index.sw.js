c5f2f1463b83edba15e08dd8ee7a9e495381a7d5
sh-navid <shamshirsaz.navid@live.com>
2023-01-14 11:11:59 +0330

    Update

--- index.sw.js
+++ index.sw.js
@@ -1,7 +1,7 @@
 // Helper link
 // https://bitsofco.de/setting-up-a-basic-service-worker/
 
-const cacheVersion = 'v1.19';
+const cacheVersion = 'v1.20';
 const cacheFiles = [
 	'./',
 	'./assets/icons/1.png?v=' + cacheVersion,
@@ -17,7 +17,7 @@ self.addEventListener('install', e => {
 	console.log('SW:: Install');
 	e.waitUntil(
 		caches.open(cacheVersion).then(cache => {
-			console.log('SW:: Caching all: app shell and content');
+			console.log('SW:: [Caching all] app shell and content');
 			return cache.addAll(cacheFiles);
 		})
 	);
@@ -27,7 +27,7 @@ self.addEventListener('fetch', e => {
 	e.respondWith(
 		caches.match(e.request).then(r => {
 			// if (e.request.url.indexOf("socket.io") > -1) return;
-			console.log('SW:: Fetching resource: ' + e.request.url);
+			console.log('SW:: [Fetching resource] ' + e.request.url);
 			return (
 				r ||
 				fetch(e.request).then(async response => {
