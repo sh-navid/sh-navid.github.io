b5d80322373ffe3d16adf9e4502334de03dde038
sh-navid <shamshirsaz.navid@live.com>
2023-01-15 10:06:09 +0330

    First Imple

---
+++ data.js
@@ -0,0 +1,482 @@
+const data = [
+    {
+        "id": 1,
+        "word": "Vigor",
+        "trans": "قدرت، توان",
+        "score": 0
+    },
+    {
+        "id": 2,
+        "word": "Guide",
+        "trans": "راهنما",
+        "score": 0
+    },
+    {
+        "id": 3,
+        "word": "Absorb",
+        "trans": "جذب کردن رطوبت یا آب",
+        "score": 0
+    },
+    {
+        "id": 4,
+        "word": "Wharf",
+        "trans": "اسکله",
+        "score": 0
+    },
+    {
+        "id": 5,
+        "word": "Event",
+        "trans": "اتفاق، مسابقه",
+        "score": 0
+    },
+    {
+        "id": 6,
+        "word": "Epidemic",
+        "trans": "همه گیر",
+        "score": 0
+    },
+    {
+        "id": 7,
+        "word": "begrime",
+        "trans": "سرزنش کردن",
+        "score": 0
+    },
+    {
+        "id": 8,
+        "word": "Obedient",
+        "trans": "مطیع",
+        "score": 0
+    },
+    {
+        "id": 9,
+        "word": "Security",
+        "trans": "امنیت، تضمین",
+        "score": 0
+    },
+    {
+        "id": 10,
+        "word": "Devour",
+        "trans": "خوردن",
+        "score": 0
+    },
+    {
+        "id": 11,
+        "word": "Summit",
+        "trans": "اوج، قله",
+        "score": 0
+    },
+    {
+        "id": 12,
+        "word": "Customary",
+        "trans": "مرسوم ،‌رایج",
+        "score": 0
+    },
+    {
+        "id": 13,
+        "word": "Unique",
+        "trans": "منحصر به فرد",
+        "score": 0
+    },
+    {
+        "id": 14,
+        "word": "Innovative",
+        "trans": "ابتکاری",
+        "score": 0
+    },
+    {
+        "id": 15,
+        "word": "Dense",
+        "trans": "فشرده، انبوه",
+        "score": 0
+    },
+    {
+        "id": 16,
+        "word": "Estimate",
+        "trans": "محاسبه کردن، قضاوت",
+        "score": 0
+    },
+    {
+        "id": 17,
+        "word": "Tempest",
+        "trans": "توفان",
+        "score": 0
+    },
+    {
+        "id": 18,
+        "word": "Dismal",
+        "trans": "غمگین",
+        "score": 0
+    },
+    {
+        "id": 19,
+        "word": "soil",
+        "trans": "خاک",
+        "score": 0
+    },
+    {
+        "id": 20,
+        "word": "Cacophony",
+        "trans": "صدای ناهنجار و خشن",
+        "score": 0
+    },
+    {
+        "id": 21,
+        "word": "Survive",
+        "trans": "جان سالم به در بردن",
+        "score": 0
+    },
+    {
+        "id": 22,
+        "word": "Wobble",
+        "trans": "لرزیدن، لرزش",
+        "score": 0
+    },
+    {
+        "id": 23,
+        "word": "Obesity",
+        "trans": "فربهی، چاقی",
+        "score": 0
+    },
+    {
+        "id": 24,
+        "word": "Utter",
+        "trans": "بر زبان آوردن",
+        "score": 0
+    },
+    {
+        "id": 25,
+        "word": "Diminish",
+        "trans": "کاهش",
+        "score": 0
+    },
+    {
+        "id": 26,
+        "word": "Delinquent",
+        "trans": "متخلف",
+        "score": 0
+    },
+    {
+        "id": 27,
+        "word": "Vicinity",
+        "trans": "نزدیکی، محله",
+        "score": 0
+    },
+    {
+        "id": 28,
+        "word": "Ignite",
+        "trans": "مشتعل شدن",
+        "score": 0
+    },
+    {
+        "id": 29,
+        "word": "Drought",
+        "trans": "خشکسالی",
+        "score": 0
+    },
+    {
+        "id": 30,
+        "word": "Tragedy",
+        "trans": "تراژدی",
+        "score": 0
+    },
+    {
+        "id": 31,
+        "word": "Verdict",
+        "trans": "رای، عقیده",
+        "score": 0
+    },
+    {
+        "id": 32,
+        "word": "Sickening",
+        "trans": "بیمار کننده، تهوع اور",
+        "score": 0
+    },
+    {
+        "id": 33,
+        "word": "Nominate",
+        "trans": "کاندید کردن",
+        "score": 0
+    },
+    {
+        "id": 34,
+        "word": "Weird",
+        "trans": "عجیب و غریب ،‌ مرموز",
+        "score": 0
+    },
+    {
+        "id": 35,
+        "word": "Indifference",
+        "trans": "بی توجهی",
+        "score": 0
+    },
+    {
+        "id": 36,
+        "word": "Brutal",
+        "trans": "وحشیانه",
+        "score": 0
+    },
+    {
+        "id": 37,
+        "word": "Discard",
+        "trans": "دور انداختن",
+        "score": 0
+    },
+    {
+        "id": 38,
+        "word": "Extract",
+        "trans": "بیرون کشیدن",
+        "score": 0
+    },
+    {
+        "id": 39,
+        "word": "Colleague",
+        "trans": "همکار",
+        "score": 0
+    },
+    {
+        "id": 40,
+        "word": "Juvenile",
+        "trans": "نوجوان",
+        "score": 0
+    },
+    {
+        "id": 41,
+        "word": "Recline",
+        "trans": "تکیه کردن",
+        "score": 0
+    },
+    {
+        "id": 42,
+        "word": "Coward",
+        "trans": "ترسو",
+        "score": 0
+    },
+    {
+        "id": 43,
+        "word": "Quarter",
+        "trans": "یک چهارم، محله",
+        "score": 0
+    },
+    {
+        "id": 44,
+        "word": "Oral",
+        "trans": "شفاهی، دهانی",
+        "score": 0
+    },
+    {
+        "id": 45,
+        "word": "Obnoxious",
+        "trans": "ناراحت کننده - شدیدا ناخوشایند",
+        "score": 0
+    },
+    {
+        "id": 46,
+        "word": "Talent",
+        "trans": "استعداد",
+        "score": 0
+    },
+    {
+        "id": 47,
+        "word": "Tyrant",
+        "trans": "ظالم",
+        "score": 0
+    },
+    {
+        "id": 48,
+        "word": "Burden",
+        "trans": "مسئولیت سنگین",
+        "score": 0
+    },
+    {
+        "id": 49,
+        "word": "Soothe",
+        "trans": "آرام کردن، تسکین دادن",
+        "score": 0
+    },
+    {
+        "id": 50,
+        "word": "agenda",
+        "trans": "دستور جلسه",
+        "score": 0
+    },
+    {
+        "id": 51,
+        "word": "Wager",
+        "trans": "شرط بندی",
+        "score": 0
+    },
+    {
+        "id": 52,
+        "word": "agency",
+        "trans": "آژانس",
+        "score": 0
+    },
+    {
+        "id": 53,
+        "word": "Vaccinate",
+        "trans": "واکسن زدن",
+        "score": 0
+    },
+    {
+        "id": 54,
+        "word": "Confident",
+        "trans": "مطمئن",
+        "score": 0
+    },
+    {
+        "id": 55,
+        "word": "Victorious",
+        "trans": "پیروز",
+        "score": 0
+    },
+    {
+        "id": 56,
+        "word": "Urban",
+        "trans": "شهری",
+        "score": 0
+    },
+    {
+        "id": 57,
+        "word": "Envy",
+        "trans": "حسادت",
+        "score": 0
+    },
+    {
+        "id": 58,
+        "word": "Plea",
+        "trans": "تقاضا",
+        "score": 0
+    },
+    {
+        "id": 59,
+        "word": "Mute",
+        "trans": "خاموش، بی صدا",
+        "score": 0
+    },
+    {
+        "id": 60,
+        "word": "Prohibit",
+        "trans": "قدغن کردن",
+        "score": 0
+    },
+    {
+        "id": 61,
+        "word": "based on",
+        "trans": "بر اساس",
+        "score": 0
+    },
+    {
+        "id": 62,
+        "word": "Deprive",
+        "trans": "محروم کردن از",
+        "score": 0
+    },
+    {
+        "id": 63,
+        "word": "Reckless",
+        "trans": "بی احتیاط",
+        "score": 0
+    },
+    {
+        "id": 64,
+        "word": "Denounce",
+        "trans": "محکوم کردن",
+        "score": 0
+    },
+    {
+        "id": 65,
+        "word": "Appropriate",
+        "trans": "مناسب، درست",
+        "score": 0
+    },
+    {
+        "id": 66,
+        "word": "Narcotic",
+        "trans": "ماده مخدر",
+        "score": 0
+    },
+    {
+        "id": 67,
+        "word": "Undeniable",
+        "trans": "غیر قابل انکار",
+        "score": 0
+    },
+    {
+        "id": 68,
+        "word": "basement",
+        "trans": "زیر زمین",
+        "score": 0
+    },
+    {
+        "id": 69,
+        "word": "Neglect",
+        "trans": "غفلت کردن از",
+        "score": 0
+    },
+    {
+        "id": 70,
+        "word": "tremendous",
+        "trans": "عظیم، بزرگ",
+        "score": 0
+    },
+    {
+        "id": 71,
+        "word": "Majority",
+        "trans": "اکثریت",
+        "score": 0
+    },
+    {
+        "id": 72,
+        "word": "Retain",
+        "trans": "حفظ",
+        "score": 0
+    },
+    {
+        "id": 73,
+        "word": "Casual",
+        "trans": "اتفاقی",
+        "score": 0
+    },
+    {
+        "id": 74,
+        "word": "Vital",
+        "trans": "ضروری، حیاتی",
+        "score": 0
+    },
+    {
+        "id": 75,
+        "word": "Cautious",
+        "trans": "محتاط",
+        "score": 0
+    },
+    {
+        "id": 76,
+        "word": "Commend",
+        "trans": "تحسین کردن",
+        "score": 0
+    },
+    {
+        "id": 77,
+        "word": "humor",
+        "trans": "شوخ‌طبعی",
+        "score": 0
+    },
+    {
+        "id": 78,
+        "word": "labor",
+        "trans": "کار یدی",
+        "score": 0
+    },
+    {
+        "id": 79,
+        "word": "Client",
+        "trans": "موکل، مشتری",
+        "score": 0
+    },
+    {
+        "id": 80,
+        "word": "Depart",
+        "trans": "ترک کردن، فوت کردن",
+        "score": 0
+    }
+];
